5b77743403374129c5bc54a15b19c654c5202414
ynan3000 <137062240+ynan3000@users.noreply.github.com>
2024-08-05 07:06:49 +0300

    Delete keep_alive.js

--- keep_alive.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const app = express();
-const port = 2323;
-app.get('/', (req, res) => res.send('The project is alive!'));
-app.listen(port, () => console.log(`Samoraii Bot is listening to http://localhost:${port}`));
-
